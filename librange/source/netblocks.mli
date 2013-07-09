0932551a7bbff18c59cb6a385a503292bb23c074
Evan Miller <eam@frap.net>
2013-07-09 13:29:35 -0700

    nuke all but libcrange

--- librange/source/netblocks.mli
+++
@@ -1,3 +0,0 @@
-val find_netblock : Tinydns.ip -> (string * string) option
-val clear_cache : unit -> unit
-val vlans_for_dc : string -> string list
--- python_seco_range/source/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from distutils.core import setup
-setup(name='seco/range',
-      version = '1.0',
-      packages = [ 'seco', 'seco.range', 'seco.range.sync' ])
-      
-      
