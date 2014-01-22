ba26ca7fad7cc275539ec62e420e1b186364c908
Evan Miller <evan@squareup.com>
2014-01-22 13:54:40 -0800

    destdir for testit.sh

--- source/testit.sh
+++ source/testit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export DESTDIR="${HOME}/prefix"
-export PATH="$DESTDIR/usr/bin:$PATH"
+export DESTDIR=${DESTDIR-"${HOME}/prefix"}
+export PATH="DESTDIR/bin:$DESTDIR/usr/bin:$PATH"
 export LD_LIBRARY_PATH="$DESTDIR/usr/lib" #FIXME should be lib64 for a 64bit build
 export DYLD_LIBRARY_PATH="$LD_LIBRARY_PATH" # OSX
 export PYTHONPATH="../python:$DESTDIR/var/libcrange/python"
